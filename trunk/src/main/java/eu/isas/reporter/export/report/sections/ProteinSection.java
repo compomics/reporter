b509e609aeeef143c527539291f714a1e7d44b13
harald.barsnes <harald.barsnes@f4f188c1-7d82-3405-8ff2-74680ddd093c>
2014-04-24 18:51:12 +0000

    Added handling of a MathException.

--- trunk/src/main/java/eu/isas/reporter/export/report/sections/ProteinSection.java
+++ trunk/src/main/java/eu/isas/reporter/export/report/sections/ProteinSection.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eu.isas.reporter.export.report.sections;
 
 import com.compomics.util.experiment.identification.Identification;
@@ -126,6 +121,7 @@ public class ProteinSection {
      * @throws ClassNotFoundException
      * @throws InterruptedException
      * @throws MzMLUnmarshallerException
+     * @throws org.apache.commons.math.MathException
      */
     public void writeSection(Identification identification, IdentificationFeaturesGenerator identificationFeaturesGenerator, QuantificationFeaturesGenerator quantificationFeaturesGenerator, ReporterIonQuantification reporterIonQuantification,
             SearchParameters searchParameters, AnnotationPreferences annotationPreferences, ArrayList<String> keys, int nSurroundingAas, WaitingHandler waitingHandler)
