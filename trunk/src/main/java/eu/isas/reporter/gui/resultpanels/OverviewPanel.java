f4cfabae3f3c2aaeff3ad6d399da5b4f3b4c0e61
harald.barsnes <harald.barsnes@f4f188c1-7d82-3405-8ff2-74680ddd093c>
2015-08-07 22:21:14 +0000

    Sped up the protein filtering by implementing batch loading. Added better progress display when clustering the data (now split into: filtering, getting the quantification data and clustering).

--- trunk/src/main/java/eu/isas/reporter/gui/resultpanels/OverviewPanel.java
+++ trunk/src/main/java/eu/isas/reporter/gui/resultpanels/OverviewPanel.java
@@ -26,7 +26,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTable;
