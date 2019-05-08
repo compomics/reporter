e6d4e564af69f67829b273ff0245586c7806718c
Harald Barsnes <harald.barsnes@gmail.com>
2019-05-08 14:20:36 +0200

    The paths are now updated before the IdentificationParametersCLI is executed. Updated PeptideShaker to version 1.16.39. Moved project to version 0.7.18.

---
+++ src/main/java/eu/isas/reporter/cli/IdentificationParametersCLI.java
@@ -0,0 +1,74 @@
+package eu.isas.reporter.cli;
+
+import com.compomics.cli.identification_parameters.AbstractIdentificationParametersCli;
+import com.compomics.software.CompomicsWrapper;
+import com.compomics.util.gui.waiting.waitinghandlers.WaitingHandlerCLIImpl;
+import com.compomics.util.waiting.WaitingHandler;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+/**
+ * The SearchParametersCLI allows creating search parameters files using command
+ * line arguments.
+ *
+ * @author Marc Vaudel
+ */
+public class IdentificationParametersCLI extends AbstractIdentificationParametersCli {
+
+    /**
+     * The waiting handler.
+     */
+    private WaitingHandler waitingHandler;
+    
+    /**
+     * Construct a new SearchParametersCLI runnable from a list of arguments.
+     * When initialization is successful, calling "run" will write the created
+     * parameters file.
+     *
+     * @param args the command line arguments
+     */
+    public IdentificationParametersCLI(String[] args) {
+        try {
+            waitingHandler = new WaitingHandlerCLIImpl();
+            // check if there are updates to the paths
+            String[] nonPathSettingArgsAsList = PathSettingsCLI.extractAndUpdatePathOptions(args);
+            initiate(nonPathSettingArgsAsList);
+        } catch (ParseException ex) {
+            waitingHandler.appendReport("An error occurred while running the command line.", true, true);
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Starts the launcher by calling the launch method. Use this as the main
+     * class in the jar file.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            new IdentificationParametersCLI(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void createOptionsCLI(Options options) {
+        ReporterCLIParameters.createOptionsCLI(options);
+    }
+
+    @Override
+    protected String getOptionsAsString() {
+        return ReporterCLIParameters.getOptionsAsString();
+    }
+
+    /**
+     * Returns the path to the jar file.
+     *
+     * @return the path to the jar file
+     */
+    public String getJarFilePath() {
+        return CompomicsWrapper.getJarFilePath(this.getClass().getResource("IdentificationParametersCLI.class").getPath(), "Reporter");
+    }
+}
